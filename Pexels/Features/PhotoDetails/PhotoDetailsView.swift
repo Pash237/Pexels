837dbe81bacb8ff73f43af5aa59a060411b8fc10
Pavel Alexeev <pasha.alexeev@gmail.com>
2024-06-09 18:23:32 +0200

    Feed details

---
+++ Pexels/Features/PhotoDetails/PhotoDetailsView.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoDetailsView.swift
+//  Pexels
+//
+//  Created by Pavel Alexeev on 08.06.2024.
+//
+
+import SwiftUI
+
+struct PhotoDetailsView: View {
+	let photo: Photo
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			PhotoAsyncImage(url: photo.thumbnailUrl)
+				.aspectRatio(photo.aspectRatio, contentMode: .fit)
+				.accessibilityLabel(photo.altText ?? "Photo")
+			
+			VStack(alignment: .leading) {
+				Text(photo.photographer.name)
+				if let altText = photo.altText {
+					Text(altText)
+						.foregroundStyle(.secondary)
+				}
+			}
+			.padding(.horizontal)
+			
+			Spacer()
+		}
+		.navigationBarTitleDisplayMode(.inline)
+	}
+}
+
+#Preview {
+	NavigationStack {
+		PhotoDetailsView(photo: .mock)
+	}
+}
